1ab9d96084cde3a9a777b84e0fcf3a16e69ab027
sohamkundu27 <sohamkundu2704@gmail.com>
2026-01-07 17:14:00 -0600

    perceptron training fix

--- nvidiaDL/chp1.py
+++ nvidiaDL/chp1.py
@@ -1,30 +1,49 @@
 import random
-# perceptron function
+# perceptron function 
 # Sign or Signum function 
+# takes in 2 mats, this is a dot product between, so there is one weight 
+random.seed(42)
 def compute_output(w, x):
     z = 0
     for i in range(len(w)):
         z += x[i] * w[i]
     return 1 if z >= 0 else -1
 
-w = [0.9,0.6,0.5]
-x = [1,1,-1]
-y = [-1, -1, -1] # actual 
-lr = 0.01
-print(compute_output(w,x))
+# A list of input VECTORS (each has 3 features for example)
+X_train = [
+    [1,  1, -1],
+    [1, -1,  1],
+    [-1, 1,  1]
+]
 
-def perceptron_training(w, x, y, lr):
+# A list of TARGETS (one for each vector)
+Y_train = [1, -1, 1]
+
+# Weights (same length as a single input vector)
+w = [0.1, 0.2, -0.5]
+lr = 0.1
+# print(compute_output(w,x))
+# takes a list of features(X)
+def perceptron_training(w, X, Y, lr):
     all_correct = False
     while not all_correct:
         all_correct = True
         
-        indices = [i for i in range(len(w))]
-        random.shuffle(indices)
-        for i in indices:
-            pred = compute_output(w, x)
-            if pred != y[i]:
-                w[i] += float(y[i]) * lr * float(x[i]) 
+        # Iterate through the SAMPLES (the rows of data), not the indices of weights
+        for i in range(len(X)):
+            current_input = X[i]
+            target = Y[i]
+            
+            # Predict using the current input vector
+            pred = compute_output(w, current_input)
+            
+            if pred != target:
                 all_correct = False
-        return w
+                # Update ALL weights based on this error
+                # We need to loop through weights to update them
+                for j in range(len(w)):
+                    w[j] += lr * target * current_input[j]
+                    
+    return w
 
-print(perceptron_training(w, x, y, lr))
+print(perceptron_training(w, X_train, Y_train, lr))
