ca8439917b077c197583c3361bc0abb113eea07b
sohamkundu27 <sohamkundu2704@gmail.com>
2026-01-07 16:44:54 -0600

    perception training loop functionality

--- nvidiaDL/chp1.py
+++ nvidiaDL/chp1.py
@@ -1,4 +1,4 @@
-
+import random
 # perceptron function
 # Sign or Signum function 
 def compute_output(w, x):
@@ -9,5 +9,22 @@ def compute_output(w, x):
 
 w = [0.9,0.6,0.5]
 x = [1,1,-1]
-
+y = [-1, -1, -1] # actual 
+lr = 0.01
 print(compute_output(w,x))
+
+def perceptron_training(w, x, y, lr):
+    all_correct = False
+    while not all_correct:
+        all_correct = True
+        
+        indices = [i for i in range(len(w))]
+        random.shuffle(indices)
+        for i in indices:
+            pred = compute_output(w, x)
+            if pred != y[i]:
+                w[i] += float(y[i]) * lr * float(x[i]) 
+                all_correct = False
+        return w
+
+print(perceptron_training(w, x, y, lr))
