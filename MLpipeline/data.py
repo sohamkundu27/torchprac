2cc112320cca56f0aad6e958df7b928d0f3d8c74
sohamkundu27 <sohamkundu2704@gmail.com>
2026-06-04 23:17:18 -0500

    requirements and more

--- MLpipeline/data.py
+++ MLpipeline/data.py
@@ -1,14 +1,19 @@
-import pytorch from torch.utils.data import Dataset, DataLoader
+import torch 
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from torchvision import datasets, transforms
 
 # gimme mnist and put it in a dataset variable|
 def get_mnist_data(batch_size=64):
     transform = transforms.Compose([transforms.ToTensor()])
+    # Download and load the training and test datasets
     train_dataset = datasets.MNIST(root='', train=True, download=True, transform=transform)
     test_dataset = datasets.MNIST(root='', train=False, download=True, transform=transform)
 
+    # dataloaders handle the batch distribution and shuffling of the data
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
+
+get_mnist_data()
